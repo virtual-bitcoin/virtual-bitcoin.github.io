4ff930c6607eb25715ef1e79240ba26e85eb615d
yj.gaia <gaiaprotocolnft@gmail.com>
2025-07-21 19:50:46 +0900

    Remove LimitChunkCountPlugin from webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
     new IgnoreEmitPlugin(['styles.js']),
   ],
